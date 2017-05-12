6d728bec43a34c3961f9252eb75065ef2f7e75f0
Maksim Teplyy <maksim.teplyy@teamvoy.com>
2017-05-12 18:04:33 +0300

    Color scheme chosen

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {
-  green300, cyan700
+  green300, green800
 } from 'material-ui/styles/colors';
 /* Elements */
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Paper from 'material-ui/Paper';
 /* Events (use instead of onClick) */
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -18,19 +19,53 @@ const muiTheme = getMuiTheme({
 });
 
 // Sources: http://www.material-ui.com/#/get-started/usage
+// Sources 2: https://github.com/TeplyyMaksim/react-test-2/blob/master/note-app/notes.js
+
 export default React.createClass({
-  handleClickTouch() {
-    console.log(muiTheme.palette.primary1Color);
+  getInitialState: function () {
+    return {
+      counts: [],
+      displayedCounts: [],
+      newCount: {}
+    };
+  },
+
+  addCount() {
+    const counts = this.state.counts.slice(),
+      newCount = this.state.newCount;
+    counts.unshift(newCount);
+    this.setState({
+      counts
+    });
+  },
+
+  debugerClick() {
+    console.log(this.state.counts);
   },
 
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="app">
-          <h1 className="header">Count your count$ with us</h1>
-          <FloatingActionButton label="Default" onTouchTap={this.handleClickTouch}>
-            <div>+</div>
-          </FloatingActionButton>
+          <Paper zDepth={2}>
+            <header>Count your count$ with us</header>
+          </Paper>
+
+          <main>
+            <ul className="counts-list">
+
+            </ul>
+            <FloatingActionButton onTouchTap={this.addCount}>
+              <div>+</div>
+            </FloatingActionButton>
+            <FloatingActionButton onTouchTap={this.debugerClick}>
+              <div>D</div>
+            </FloatingActionButton>
+          </main>
+
+          <Paper zDepth={2}>
+            <footer>Count your count$ with us</footer>
+          </Paper>
         </div>
       </MuiThemeProvider>
     );
