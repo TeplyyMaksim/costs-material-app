c8dc8e9d03f5bff2acfb0be6ffbcc2d6a3885c6a
Maksim <teplyy.maksim@gmail.com>
2017-05-17 22:23:12 +0300

    Bottom footer added

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {
-  green300, green800
+  green300, green800,
+  yellow600
 } from 'material-ui/styles/colors';
 /* Elements */
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import FontIcon from 'material-ui/FontIcon';
 import Paper from 'material-ui/Paper';
+/* Custom Elements */
+import CountsList from './CountsList/CountsList.jsx';
 /* Events (use instead of onClick) */
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -18,8 +22,10 @@ const muiTheme = getMuiTheme({
   }
 });
 
-// Sources: http://www.material-ui.com/#/get-started/usage
-// Sources 2: https://github.com/TeplyyMaksim/react-test-2/blob/master/note-app/notes.js
+// Logic sample: https://github.com/TeplyyMaksim/react-test-2/blob/master/note-app/notes.js
+// Design (LAST PRIORITY): http://www.material-ui.com/#/get-started/usage
+// Copy button and grid like here (NOTE: AFTER LOGIC!!!): https://docs.google.com/document/u/0/
+// Icons list (THE LASTEST PRIORITY): https://material.io/icons/
 
 export default React.createClass({
   getInitialState: function () {
@@ -47,25 +53,26 @@ export default React.createClass({
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="app">
-          <Paper zDepth={2}>
-            <header>Count your count$ with us</header>
-          </Paper>
+          <div className="content-wrapper">
+            <Paper zDepth={2}>
+              <header>
+                Count your <span>count<FontIcon className="material-icons" color={yellow600}>monetization_on</FontIcon></span> with us
+              </header>
+            </Paper>
 
-          <main>
-            <ul className="counts-list">
-
-            </ul>
-            <FloatingActionButton onTouchTap={this.addCount}>
-              <div>+</div>
-            </FloatingActionButton>
-            <FloatingActionButton onTouchTap={this.debugerClick}>
-              <div>D</div>
-            </FloatingActionButton>
-          </main>
-
-          <Paper zDepth={2}>
-            <footer>Count your count$ with us</footer>
-          </Paper>
+            <main>
+              <CountsList>Hello</CountsList>
+              <FloatingActionButton onTouchTap={this.addCount}>
+                <div>+</div>
+              </FloatingActionButton>
+              <FloatingActionButton onTouchTap={this.debugerClick}>
+                <div>D</div>
+              </FloatingActionButton>
+            </main>
+            <Paper zDepth={2}>
+              <footer>Copyright (c) 2017 Teplyy Maksim All Rights Reserved.</footer>
+            </Paper>
+          </div>
         </div>
       </MuiThemeProvider>
     );
---
+++ app/components/App/CountsList/CountsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <ul>
+        <li>Hello, world</li>
+        <li>Hello, world</li>
+        <li>Hello, world</li>
+        <li>Hello, world</li>
+        <li>Hello, world</li>
+      </ul>
+    );
+  }
+});
